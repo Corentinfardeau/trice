1047e867c75d029973b56378c49be2f18cfc9b4b
corentinfardeau <fardeaucorentin@gmail.com>
2015-12-14 14:12:44 +0100

    - Xcode project

---
+++ trice/trice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  trice
+//
+//  Created by Corentin FARDEAU on 14/12/2015.
+//  Copyright © 2015 Corentin FARDEAU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
