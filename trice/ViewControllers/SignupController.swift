32a74ece70b27f3b6e849f7a1168a2fdf03c5342
corentinfardeau <fardeaucorentin@gmail.com>
2015-12-15 16:03:22 +0100

    storyboard for signup signin

---
+++ trice/ViewControllers/SignupController.swift
@@ -0,0 +1,40 @@
+//
+//  SignupController.swift
+//  trice
+//
+//  Created by Corentin FARDEAU on 15/12/2015.
+//  Copyright © 2015 Corentin FARDEAU. All rights reserved.
+//
+
+import UIKit
+
+class SignupController: UIViewController {
+
+    @IBOutlet weak var errorLabel: UILabel!
+    @IBOutlet weak var mailTextField: UITextField!
+    @IBOutlet weak var pseudoTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var signupButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
