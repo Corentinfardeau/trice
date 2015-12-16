0c43863c6c25669b8e43ab74a62ba01fb2d3a0ae
gvergnaud <gabriel.vergnaud@hetic.net>
2015-12-16 16:13:50 +0100

    update user

--- trice/Network/Api.swift
+++ trice/Network/Api.swift
@@ -102,6 +102,35 @@ class Api {
         }
     }
     
+    func editcurrentUser(username: String?, email: String?, password: String?, successCallback: (user: PFUser) -> (), errorCallback: (error: NSError) -> ()) {
+        
+        if let currentUser = PFUser.currentUser() {
+            
+            if let username = username {
+                currentUser["username"] = username
+            }
+            
+            if let email = email {
+                currentUser["email"] = email
+            }
+            
+            if let password = password {
+                currentUser["password"] = password
+            }
+            
+            currentUser.saveInBackgroundWithBlock({ (success: Bool, error: NSError?) -> Void in
+                if success {
+                    successCallback(user: currentUser)
+                    
+                } else {
+                    errorCallback(error: error!)
+                }
+            })
+            
+        }
+        
+    }
+    
 
     // MARK: - Likes
     
--- trice/ViewControllers/ProfileController.swift
+++ trice/ViewControllers/ProfileController.swift
@@ -7,16 +7,22 @@
 //
 
 import UIKit
+import Parse
 
 class ProfileController: UITableViewController {
 
+    var user: PFUser!
+    
     @IBOutlet weak var pseudoTextField: UITextField!
     @IBOutlet weak var mailTextField: UITextField!
+    @IBOutlet weak var newPasswordTextField: UITextField!
+    @IBOutlet weak var passwordConfirmationTextField: UITextField!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let user = Api.sharedInstance.getCurrentUser()
+        user = Api.sharedInstance.getCurrentUser()
         pseudoTextField.text = user?.username
         mailTextField.text = user?.email
         
@@ -28,11 +34,47 @@ class ProfileController: UITableViewController {
     }
     
     @IBAction func logoutClickAction(sender: AnyObject) {
-        
         Api.sharedInstance.logOut()
         self.dismissViewControllerAnimated(true, completion: nil)
-
     }
 
+
+    @IBAction func saveClickAction(sender: AnyObject) {
+        updateInformation()
+        updatePassword()
+    }
+    
+    
+    func updateInformation() {
+        
+        let newUsername = pseudoTextField.text != user.username ? pseudoTextField.text : nil
+        let newEmail = mailTextField.text != user.email ? mailTextField.text : nil
+        
+        if newUsername != nil || newEmail != nil {
+            
+            Api.sharedInstance.editcurrentUser(newUsername, email: newEmail, password: nil,
+                successCallback: { user in
+                    print("userupdated!")
+                },
+                errorCallback: { error in
+                    self.showAlert("Server connection error :(")
+                }
+            )
+        } else {
+            print("no change")
+        }
+    }
+    
+    func updatePassword() {
+        
+    }
+    
+    // MARK: - alert
+    
+    func showAlert(message: String) {
+        let alert = UIAlertController(title: "Alert", message: message, preferredStyle: UIAlertControllerStyle.Alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: nil))
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
     
 }
